2374cd139b76217e42be2181075a76f84f8e34c3
erichestella <erichestella05@gmail.com>
2024-02-07 01:23:23 +0800

    added some code to track hands

--- hand_language.py
+++ hand_language.py
@@ -9,3 +9,7 @@ mp_hands=mp.solutions.hands
 
 #this is the webcam 
 cap = cv2.VideoCapture(0)
+
+#its code track the hand 
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+    
