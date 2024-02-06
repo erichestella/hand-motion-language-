2c20b867d49baf049adda420f49721955aa5a241
erichestella <erichestella05@gmail.com>
2024-02-06 16:47:39 +0800

    added some mediapipe components

--- hand_language.py
+++ hand_language.py
@@ -1,3 +1,8 @@
 #libraries that needed in this code 
 import mediapipe as mp
 import cv2 
+
+#mediapipe components
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_drawing = mp.solutions.drawing_utils
+mp_hands=mp.solutions.hands
